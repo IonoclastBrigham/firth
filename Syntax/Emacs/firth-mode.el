8b32243f9851c776f1273871a565afbc00ede278
Brigham Toskin <brigham@ionoclast.com>
2015-04-20 18:53:07 -0700

    Updates to emacs mode.
    Refactor to load file only when editing forth files.
    Add copyright notice.
    Add comment giving autoload lines to add to .emacs file.
    Rename ": Firth" to ":Firth"

--- Syntax/Emacs/firth-mode.el
+++ Syntax/Emacs/firth-mode.el
@@ -1,15 +1,19 @@
-; let users hook loading firth mode
-(defvar firth-mode-hook nil)
+;; firth-mode.el
+;; :Firth source editing major mode
+;;
+;; Copyright © 2015 Brigham Toskin
+;;
+;; To automatically load firth-mode for .firth files, install firth-mode.el on
+;; your library load path and add the following lines to your .emacs file:
+;;   (autoload 'firth-mode "firth-mode" nil t)
+;;   (add-to-list 'auto-mode-alist '("\\.firth\\'" . firth-mode))
 
 ; keymap for macro keys
 (defvar firth-mode-map
   (let ((map (make-keymap)))
     (define-key map "\C-j" 'newline-and-indent)
     map)
-  "Keymap for : Firth Major Mode")
-
-; automatically load firth-mode, for .firth files
-(add-to-list 'auto-mode-alist '("\\.firth\\'" . firth-mode))
+  "Keymap for :Firth Major Mode")
 
 ;;; Syntax Highlighting ;;;
 
@@ -21,31 +25,31 @@
 
 (defface firth-consts
   '((t (:inherit font-lock-constant-face)))
-  ": Firth const font."
+  ":Firth const font."
   :group 'font-lock-faces)
 (defvar firth-consts 'firth-consts)
 
 (defface firth-defining-words
   '((t (:inherit firth-prims :weight bold)))
-  ": Firth core defining words."
+  ":Firth core defining words."
   :group 'font-lock-faces)
 (defvar firth-defining-words 'firth-defining-words)
 
 (defface firth-word-def-name
   '((t (:inherit font-lock-function-name-face :weight bold)))
-  ": Firth new word definition name."
+  ":Firth new word definition name."
   :group 'font-lock-faces)
 (defvar firth-word-def-name 'firth-word-def-name)
 
 (defface firth-ticked-word
   '((t (:inherit font-lock-keyword-face :slant oblique)))
-  "Ticked : Firth words."
+  "Ticked :Firth words."
   :group 'font-lock-faces)
 (defvar firth-ticked-word 'firth-ticked-word)
 
 (defface firth-core-words
   '((t (:inherit font-lock-keyword-face)))
-  ": Firth word def names."
+  ":Firth word def names."
   :group 'font-lock-faces)
 (defvar firth-core-words 'firth-core-words)
 
@@ -67,7 +71,7 @@
       "calls:" "calledby:" "tstart" "tend"
       "&" "|" "^"
       "NOP") 'words) (1 firth-prims)))
-  "Minimal Highlighting for : Firth")
+  "Minimal Highlighting for :Firth")
 
 (defconst firth-consts-pattern
   (list
@@ -82,18 +86,18 @@
    '("\\<[+-]?[[:digit:]]+\\>" . firth-consts)
    '("\\<[+-]?[[:digit:]]+\\.[[:digit:]]+\\(?:[Ee][+-]?[[:digit:]]+\\)?\\>" . firth-consts)
    '("\\<0[Xx][[:xdigit:]]+\\>" . firth-consts))
- "Additional constants for highlighting : Firth Mode")
+ "Additional constants for highlighting :Firth Mode")
 
 (defconst firth-comments-pattern
   (list
    '("\\<\\(//\\)\\>\\(.*\\)" (1 font-lock-comment-face) (2 font-lock-comment-face))
    '("\\<(.*)\\>" . font-lock-comment-face))
-  ": Firth standard comments.")
+  ":Firth standard comments.")
 
 (defconst firth-agregate-literals-pattern
   (list
    '("\\<{}\\|\\[\\]\\>" . firth-consts))
- "Additional literals for agregate types for : Firth Mode")
+ "Additional literals for agregate types for :Firth Mode")
 
 (defconst firth-definitions-pattern
    (list
@@ -140,7 +144,7 @@
          "NOOP") 'words)
      . firth-core-words)
    '("\\<.\"\\s-.*?\"\\>" . font-lock-string-face)) ; print string
-  "Useful words from : Firth core library.")
+  "Useful words from :Firth core library.")
 
 ;; Define some default groupings for syntax highlighting options.
 ;; Users of firth-mode may choose one of the below, or set their
@@ -149,14 +153,14 @@
   (append
    firth-prims-pattern
    firth-consts-pattern)
-  "Minimally useful syntax highlighting for : Firth")
+  "Minimally useful syntax highlighting for :Firth")
 
 (defconst firth-nicer-syntax-pattern
   (append
    firth-comments-pattern
    firth-min-syntax-pattern
    firth-agregate-literals-pattern)
-  "Nicer syntax highlighting for : Firth with agregates and standard comments.")
+  "Nicer syntax highlighting for :Firth with agregates and standard comments.")
 
 (defconst firth-full-syntax-pattern
   (append
@@ -166,10 +170,10 @@
    firth-core-words-pattern
    firth-prims-pattern
    firth-agregate-literals-pattern)
-  "Full syntax highlighting for : Firth, including defs and core lib.")
+  "Full syntax highlighting for :Firth, including defs and core lib.")
 
 (defvar firth-font-lock-keywords-pattern firth-full-syntax-pattern
-  "Default Highlighting Expression for : Firth Mode")
+  "Default Highlighting Expression for :Firth Mode")
 
 ;; override how some characters are handled by emacs
 (defvar firth-mode-syntax-table
@@ -211,16 +215,19 @@
     ;; (modify-syntax-entry ?\n "> " stab)
 
     stab)
-  "Syntax Table for : Firth Mode")
+  "Syntax Table for :Firth Mode")
 
 ;;; Indentation ;;;
 
 ;; TODO
 
-;;; : Firth Mode Init and Entry ;;;
+;;; :Firth Mode Init and Entry ;;;
+
+; let users hook loading firth mode
+(defvar firth-mode-hook nil)
 
 (defun firth-mode ()
-  "Major Mode for Editing : Firth Files"
+  "Major Mode for Editing :Firth Files."
   (interactive)
   (kill-all-local-variables)
   (set-syntax-table firth-mode-syntax-table)
@@ -228,7 +235,7 @@
   (set (make-local-variable 'font-lock-defaults) '(firth-font-lock-keywords-pattern))
 ;  (set (make-local-variable 'indent-line-function) '(firth-indent-line))
   (setq major-mode 'firth-mode)
-  (setq mode-name ": Firth")
+  (setq mode-name ":Firth")
   (run-hooks 'firth-mode-hook))
 
 (provide 'firth-mode)
