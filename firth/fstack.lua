efbd3dd3bde8974028cfaf3a377c0bca1b90cf0d
Brigham Toskin <brigham@ionoclast.com>
2015-05-08 18:32:38 -0700

    General stack cleanup, and function renaming.

--- firth/fstack.lua
+++ firth/fstack.lua
@@ -16,44 +16,76 @@ local function yankfilter(n, i, tos, ...)
 	if count(...) > 0 then return tos, yankfilter(n+1, i, ...) end
 end
 
--- stack methods --
+-- in-thread stack manipulation primitives --
 
+-- redundant, but possibly semantically useful	
 local function push(x, ...)
+	return x, ...
+end
+
+local function drop(tos, ...)
+	return ...
+end
+
+local function top(tos, ...)
+	return tos
+end
+
+local function shove(i, x, tos, ...)
+	if i == 0 then return x, tos, ... end
+	return shovefilter(0, i, x, ...)
+end
+
+local function yank(i, tos, ...)
+	if i == 0 then return ... end
+	return yankfilter(0, i, tos, ...)
+end
+
+local function peek(i, tos, ...)
+	if i == 0 then return tos end
+	return (select(i+1, tos, ...))
+end
+
+local height = count
+
+-- stack method coroutines / continuations --
+
+local function pushs(x, ...)
 	local continuation, nx = yield()
-	return continuation(nx, x, ...)
+	return continuation(nx, push(x, ...))
 end
 
-local function pop(_, ...)
-	local continuation, x = yield((...))
-	return continuation(x, select(2, ...))
+local function pops(_, ...)
+	local continuation, x = yield(top(...))
+	return continuation(x, drop(...))
 end
 
-local function shove(_, ...)
+local function shoves(_, ...)
 	local continuation, x = yield()
-	return continuation(x, shovefilter(0, ...))
+	return continuation(x, shove(...))
 end
 
-local function yank(x, ...)
+local function yanks(x, ...)
 	local continuation, nx = yield((select(x+1, ...)))
 	return continuation(nx, yankfilter(0, x, ...))
 end
 
-local function peek(x, ...)
+local function peeks(x, ...)
 	local continuation, x = yield((select(x+1, ...)))
 	return continuation(x, ...)
 end
 
-local function unpack(x, ...)
+local function unpacks(x, ...)
 	local continuation, x = yield(...)
 	return continuation(x, ...)
 end
 
-local function clear(x, ...)
+local function clears(x, ...)
 	local continuation, x = yield()
 	return continuation(x)
 end
 
-local function height(x, ...)
+local function heights(x, ...)
 	local continuation, x = yield(count(...))
 	return continuation(x, ...)
 end
@@ -70,61 +102,40 @@ local function new(...)
 	resume(stack, ...)
 
 	return {
-		push = function(x) resume(stack, push, x) end,
+		push = function(x) resume(stack, pushs, x) end,
 		pop = function()
-			local _, x = resume(stack, pop)
+			local _, x = resume(stack, pops)
 			return x
 		end,
 		shove = function(i, x)
 			shovei, shovex = i, x -- load shove registers
-			resume(stack, shove, nil)
+			resume(stack, shoves, nil)
 		end,
 		yank = function(i)
-			local _, x = resume(stack, yank, i)
+			local _, x = resume(stack, yanks, i)
 			return x
 		end,
 		peek = function(i)
-			local _, x = resume(stack, peek, i)
+			local _, x = resume(stack, peeks, i)
 			return x
 		end,
-		unpack = function() return select(2, resume(stack, unpack)) end,
-		clear = function() resume(stack, clear) end,
+		unpack = function() return select(2, resume(stack, unpacks)) end,
+		clear = function() resume(stack, clears) end,
 		height = function()
-			local _, h = resume(stack, height)
+			local _, h = resume(stack, heights)
 			return h
 		end,
 	}
 end
 
-local function spush(x, ...)
-	return x, ...
-end
-
-local function sdrop(tos, ...)
-	return ...
-end
-
-local function sshove(i, x, ...)
-	return shovefilter(0, i, x, ...)
-end
-
-local function syank(i, ...)
-	return yankfilter(0, i, ...)
-end
-
-local function stop(tos, ...)
-	return tos
-end
-
-local function sheight(...)
-	return select("#", ...)
-end
-
 -- export
 firth = firth or {}
-firth.fstack = { new = new,
-	spush = spush, sdrop = sdrop,
-	sshove = sshove, syank = syank,
-	stop = stop, sheight = sheight,
+firth.fstack = {
+	new = new;
+
+	push = push, drop = drop, top = top;
+	shove = shove, yank = yank, peek = peek;
+
+	height = height;
 }
 return firth.fstack
